0b3c3b9cd2a5dcc83df11663be49725640109bc8
John Weis <weis.john@gmail.com>
2015-07-22 17:29:28 -0400

    initial checkin of mongoose-address

---
+++ index.js
@@ -0,0 +1,13 @@
+
+module.exports = function(schema) {
+
+    schema.add({
+        address_line1: String,
+        address_line2: String,
+        address_city: String,
+        address_state: String,
+        address_zip: String,
+        address_country: String,
+    });
+
+}
